e6b00c5cbc49a397bf1d27b866f09cd8b77d33c5
Manjunath Pandava <pandavamanju@yahoo.co.in>
2016-12-19 23:45:38 +0530

    adding POM dependencies and log config

---
+++ flights/src/main/java/com/ctrip/pages/Test.java
@@ -0,0 +1,14 @@
+package com.ctrip.pages;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by manjunath.pandava on 19/12/16.
+ */
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        log.info("test");
+    }
+}
--- flights/src/test/java/com/ctrip/tests/FlightsTests.java
+++ flights/src/test/java/com/ctrip/tests/FlightsTests.java
@@ -1,7 +1,17 @@
 package com.ctrip.tests;
 
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
 /**
  * Created by manjunath.pandava on 19/12/16.
  */
+@Slf4j
 public class FlightsTests {
+
+    @Test(description = "Test roundtrip flight booking", groups={"P0", "Regression"}, enabled = true)
+    public void roundTripTest(){
+        log.info("Start Test");
+
+    }
 }
